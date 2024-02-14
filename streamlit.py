7c61a57b7a8d4133ec936456c7c8e3dc189a1e93
moazzampasha <157400754+moazzampasha@users.noreply.github.com>
2024-02-14 21:11:33 +0500

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title("Welcome Bano Qabil")
 st.write("Pythan CIT  by Sir Ghufran Kamaluddin")
-st.title("Campus: PCHMC")
+st.title("Campus: homopathic college NAZIMABAD")
 st.title("timing 8 to 10")
